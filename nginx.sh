76e7d88242b24c2150f57f3c903c09670a821e17
Danil0446 <58527671+Danil0446@users.noreply.github.com>
2025-03-12 23:12:01 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,21 +1,44 @@
 #!/bin/sh
-apt-get update
- apt-get install nginx
+
+echo "Установка Nginx"
+apt-get update && apt-get install nginx -y
 systemctl enable --now nginx.service
-cd /etc/nginx/sites-available.d/
-touch site.conf
-echo >> "server {
- listen 80;
-  server_name localhost .local 10.0.2.11;
 
-  access_log /var/log/nginx/access.log;
-  error_log /var/log/nginx/error.log;
+echo "Создание конфигурации"
+mkdir -p /etc/nginx/sites-available.d
+cat > /etc/nginx/sites-available.d/site.conf <<EOF
+server {
+    listen 80;
+    server_name localhost .local 10.0.2.11;
 
-  location / {
-    root /var/www/html/;
-    autoindex on;
-   }
-}"
-ln -s /etc/nginx/sites-available.d/site.conf /etc/nginx/sites-enabled.d/site.conf
-mkdir -p /var/www/html && cd /var/www/html && touch index.html
-echo >> "<html><body><h1>It works! Nginx</h1></body></html>"  /var/www/html/index.html
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+
+    location / {
+        root /var/www/html/;
+        autoindex on;
+    }
+}
+EOF
+
+echo "Активация конфига"
+mkdir -p /etc/nginx/sites-enabled.d
+ln -sf /etc/nginx/sites-available.d/site.conf /etc/nginx/sites-enabled.d/
+
+echo "Создание веб-контента"
+mkdir -p /var/www/html
+cat > /var/www/html/index.html <<EOF
+<html>
+<body>
+<h1>It works! Nginx</h1>
+</body>
+</html>
+EOF
+
+systemctl restart nginx.service
+rm -rf /root/nginx.sh
+apt-get remove git -y
+history -c  
+
+
+
